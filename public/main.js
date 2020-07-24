f0e530d14e176c039b237c825fe23609a5225d18
HARUKI,MIDORIKAWA <218c0422@s.obirin.ac.jp>
2020-07-24 23:03:06 +0900

    時間、参加時の通知色の追加

--- public/main.js
+++ public/main.js
@@ -8,121 +8,64 @@ const name = document.getElementById("name");
 
 let username='';
 nameform.addEventListener('submit', function(event){
-  username = name.value;
+  if(name.value!==''){
+    username = name.value;
+    nameform.style.display ="none";
+    form.style.display ="block";
+
+    socketio.emit('signin');
+
+    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
+    socketio.emit('message', msg);
+  }
+
   event.preventDefault();
-  nameform.style.display ="none";
-  form.style.display ="block";
 })
 
 form.addEventListener('submit', function(event){
-  const msg = JSON.stringify({msg: input.value, name: username})
-  socketio.emit('message', msg);
-  input.value='';
+  if(input.value!==''){
+    const msg = {msg: input.value, name: username};
+    socketio.emit('message', msg);
+    input.value='';
+  }
   event.preventDefault();
 })
-socketio.on('message',function(msg){
-  const obj = JSON.parse(msg);
 
-  const dt = document.createElement("dt");
-  const dd = document.createElement("dd");
-  dt.append(obj.name);
-  chats.append(dt);
-  dd.append(obj.msg);
-  chats.append(dd);
+socketio.on('message',function(msg){
+  if(username===''){
+    // まだ参加していなかったら、チャットを表示しない
+    return;
+  }
+  displayMessage(msg);
 });
 
+// 参加時に過去のメッセージを受け取る
+socketio.on('signin',function(msgs){
+  for(let i=0;i<msgs.length;i++){
+    const msg = msgs[i];
+    displayMessage(msg);
+  }
+});
 
+function displayMessage(msg){
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  const dd2 = document.createElement("dd");
+  dt.append(msg.name);
+  chats.append(dt);
+  dd.append(msg.msg);
+  chats.append(dd);
+  dd2.append(formatDate(new Date(msg.date), 'yyyy/MM/dd HH:mm:ss'));
+  chats.append(dd2);
+}
 
-
-
-
-
-
-
-
-
-
-
-'use strict';
-
-// モジュール
-const express = require( 'express' );
-const http = require( 'http' );
-const socketIO = require( 'socket.io' );
-
-// オブジェクト
-const app = express();
-const server = http.Server( app );
-const io = socketIO( server );
-
-// 定数
-const PORT = process.env.PORT || 1337;
-
-// 関数
-// 数字を２桁の文字列に変換
-const toDoubleDigitString =
-    ( num ) =>
-    {
-        return ( "0" + num ).slice( -2 );   // slice( -2 )で後ろから２文字取り出す。
-    };
-
-// 時刻文字列の作成（書式は「YY/DD/MM hh:mm ss」）
-const makeTimeString =
-    ( time ) =>
-    {
-        return toDoubleDigitString( time.getFullYear() ) + '/' + toDoubleDigitString( time.getMonth() + 1 ) + '/' + toDoubleDigitString( time.getDate() )
-            + ' ' + toDoubleDigitString( time.getHours() ) + ':' + toDoubleDigitString( time.getMinutes() ) + ' ' + toDoubleDigitString( time.getSeconds() );
-    }
-
-// 接続時の処理
-// ・サーバーとクライアントの接続が確立すると、
-// 　サーバー側で、'connection'イベント
-// 　クライアント側で、'connect'イベントが発生する
-io.on(
-    'connection',
-    ( socket ) =>
-    {
-        console.log( 'connection' );
-
-        // 切断時の処理
-        // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
-        socket.on(
-            'disconnect',
-            () =>
-            {
-                console.log( 'disconnect' );
-            } );
-
-        // 新しいメッセージ受信時の処理
-        // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
-        socket.on(
-            'new message',
-            ( strMessage ) =>
-            {
-                console.log( 'new message', strMessage );
-
-                // 現在時刻の文字列の作成
-                const strNow = makeTimeString( new Date() );
-
-                // メッセージオブジェクトの作成
-                const objMessage = {
-                    strMessage: strMessage,
-                    strDate: strNow
-                }
-
-                // 送信元含む全員に送信
-                //io.emit( 'spread message', strMessage );
-                io.emit( 'spread message', objMessage );
-            } );
-    } );
-
-// 公開フォルダの指定
-app.use( express.static( __dirname + '/public' ) );
-
-// サーバーの起動
-server.listen(
-    PORT,
-    () =>
-    {
-        console.log( 'Server on port %d', PORT );
-    } );
+function formatDate (date, format) {
+  format = format.replace(/yyyy/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
+  return format;
+};
